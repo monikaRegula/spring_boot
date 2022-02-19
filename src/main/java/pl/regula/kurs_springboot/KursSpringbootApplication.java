780d77de1d545e6d492d363f9c223d8be4d76338
monikaRegula <monikaregula97@gmail.com>
2022-02-19 18:42:19 +0100

    first commit

---
+++ src/main/java/pl/regula/kurs_springboot/KursSpringbootApplication.java
@@ -0,0 +1,13 @@
+package pl.regula.kurs_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/regula/kurs_springboot/KursSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.regula.kurs_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
